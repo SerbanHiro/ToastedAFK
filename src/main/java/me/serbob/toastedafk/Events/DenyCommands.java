49c344da2ecaa07678992c3432e67476fb7f5139
SerbanHiro <51057957+SerbanHiro@users.noreply.github.com>
2023-04-05 14:04:02 +0300

    1.6.1

--- src/main/java/me/serbob/toastedafk/Events/DenyCommands.java
+++ src/main/java/me/serbob/toastedafk/Events/DenyCommands.java
@@ -7,13 +7,17 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
+import static me.serbob.toastedafk.Managers.ValuesManager.afkTimers;
+
 public class DenyCommands implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void PreProcessCommand(PlayerCommandPreprocessEvent event) {
-        String cmd = event.getMessage();
-        if(ToastedAFK.instance.getConfig().getStringList("denied_commands").contains(cmd)) {
-            event.getPlayer().sendMessage(AFKUtil.c(ToastedAFK.instance.getConfig().getString("on_command_deny")));
-            event.setCancelled(true);
+        if(afkTimers.containsKey(event.getPlayer())) {
+            String cmd = event.getMessage();
+            if (ToastedAFK.instance.getConfig().getStringList("denied_commands").contains(cmd)) {
+                event.getPlayer().sendMessage(AFKUtil.c(ToastedAFK.instance.getConfig().getString("on_command_deny")));
+                event.setCancelled(true);
+            }
         }
     }
 }
